4b5269c6e9b26eb70e380943cf33f8d932547cb4
bureauvictor <82405991+bureauvictor@users.noreply.github.com>
2025-10-06 08:12:04 +0200

    Refactor greeting function and add main execution
    Comment out the GreetTheUser function and its call, and add a main function that greets the user using their environment username.

--- Hola-Mundo.py
+++ Hola-Mundo.py
@@ -1,11 +1,18 @@
 import os
 
 
-def GreetTheUser():
-  username = ''
+#def GreetTheUser():
+ # username = ''
   
-  print(f'I am a {username} using github. Just want to greet you!!')
+  #print(f'I am a {username} using github. Just want to greet you!!')
 
+
+#if __name__ == "__main__":
+ #   GreetTheUser()
+
+def main():
+    nombre = os.getenv('USERNAME', 'usuario')
+    print(f"Hola {nombre} desde GitHub")
 
 if __name__ == "__main__":
-    GreetTheUser()
+    main()
